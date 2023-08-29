ec922fb2f9e4b218e971dfa9408165e7d508cc10
mh0316 <85312998+mh0316@users.noreply.github.com>
2023-08-29 09:23:16 -0400

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -9,6 +9,15 @@
 #		  mundo 44
 #La salida debe ser
 #         ('mundo', 44, 20, 90, 'hola', 'mundo', 44)
-t = input()
-m = input()
-print(m)
+t = input().split()
+m = input().split()
+
+valor1 = t1[0]
+valor2 = int(t1[1])
+valor3 = t1[2]
+valor4 = t2[0]
+valor5 = int(t2[1])
+
+tupla = (f"('{valor4}', {valor5}, {valor1}, {valor2}, '{valor3}', '{valor4}', {valor5})")
+
+print(tupla)
